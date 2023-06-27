fbf62e14ceef74c3c3a8195a2aba9d146486c094
1Snake001 <streetstylecr3w@gmail.com>
2023-06-27 11:53:29 +0200

    delete async function in services was implemented

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,35 +1,51 @@
 import { db } from "../firebase-config/firebase";
-import { collection, addDoc, getDocs} from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 const userRef = collection(db, "users");
 
 interface InputValue {
-    name: string;
-    email: string;
-    address: string;
-  }
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+}
 
-  class UserServices {
-    getAllUsers = async () => {
-      try {
-        return await getDocs(userRef);
-      } catch (error) {
-        // Handle the error
-        console.error('Error getting all users:', error);
-        throw error;
-      }
-    };
-  
-    addNewUser = async (newUser: InputValue) => {
-      try {
-        return await addDoc(userRef, newUser);
-      } catch (error) {
-        // Handle the error
-        console.error('Error adding a new user:', error);
-        throw error;
-      }
-    };
-  }
-  
+class UserServices {
+  getAllUsers = async () => {
+    try {
+      return await getDocs(userRef);
+    } catch (error) {
+      // Handle the error
+      console.error("Error getting all users:", error);
+      throw error;
+    }
+  };
+
+  addNewUser = async (newUser: InputValue) => {
+    try {
+      return await addDoc(userRef, newUser);
+    } catch (error) {
+      // Handle the error
+      console.error("Error adding a new user:", error);
+      throw error;
+    }
+  };
+  deleteUser = async (id: string) => {
+    try {
+      const userDoc = doc(userRef, id);
+      return await deleteDoc(userDoc);
+    } catch (error) {
+      // Handle the error
+      console.error("Error adding a new user:", error);
+      throw error;
+    }
+  };
+}
 
 export default UserServices;
