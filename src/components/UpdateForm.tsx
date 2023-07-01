64bba3a90b344ba79e51d87763e08d79eb314190
1Snake001 <streetstylecr3w@gmail.com>
2023-07-01 15:03:07 +0200

    get user by id function was implemented

---
+++ src/components/UpdateForm.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Form from "./Form";
+import { FormProps } from "./Form";
+import UserServices from "../services/services";
+
+const UpdateForm: React.FC<FormProps> = ({
+  getUserData,
+  inputValues,
+  setInputValues,
+}) => {
+  const [user, setUser] = useState({})
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    if (id) {
+      const userService = new UserServices();
+
+    userService
+        .getUserById(id)
+        .then((user) => {
+         
+          setUser(user);
+        })
+        .catch((error) => {
+   
+          console.error(error);
+        });
+    }
+  }, [id]);
+
+  return (
+    <>
+      <Form
+        getUserData={getUserData}
+        inputValues={inputValues}
+        setInputValues={setInputValues}
+        type="edit"
+        id={id}
+        user={user}
+      />
+    </>
+  );
+};
+
+export default UpdateForm;
